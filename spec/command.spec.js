97b35908c5fc0cc234f6ce595eb78e9c79af1850
Grant Tracey <gtracey24@gmail.com>
2024-02-22 21:09:48 -0600

    Test 1, 2, 3 passing

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,6 +5,7 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
+// TEST 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
@@ -12,17 +13,18 @@ describe("Command class", function() {
 // TEST 2
 // Create a second Command test using “constructor sets command type” as the description.
 // This test checks that the constructor in the Command class correctly sets the commandType property in the new object.
+  it('constructor sets command type', function () {
+    let constructorOne = new Command(1, 2);
+    expect(constructorOne.commandType).toBe(1);
+  })
 
-  it('constructor sets commandType', function () {
-    let type = new Command(test)
-    expect(type.commandType).not.toBeUndefined();
-  });
-
-  // TEST 3
-
-  it('constructor sets a value passed in as the 2nd argument', function () {
-    const obj = new Command(1, 2);
-    expect(obj.value).toBe(2);
-  });
+// TEST 3
+// Code a third test using “constructor sets a value passed in as the 2nd argument” as the description.
+// This test checks that the constructor correctly sets the value property in the new object.
+// You may not need to know a proper value in order to write this test.
+ it('constructor sets a value passed in as the 2nd argument', function () {
+  let constructorTwo = new Command(1, 2);
+    expect(constructorTwo.value).toBe(2);
+ })
 
 });
