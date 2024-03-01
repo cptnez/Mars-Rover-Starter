b073811f220d931c832d8c6eaa0be0032480de84
Grant Tracey <gtracey24@gmail.com>
2024-02-29 23:06:11 -0600

    All tests passing following cleaning up.

--- message.js
+++ message.js
@@ -1,29 +1,8 @@
-// CLASSES
-  //!! classes are blueprints for building multiple objects of the same type. AKA Classes build Objects!
-  //Each time the Astronaut class is called,
-  //constructor builds an object with the SAME set of keys, but it assigns different values to the keys based on the arguments.
-  // Use the keyword 'new' to create and OBJECT from the CLASS
-
-    // CONTRUCTORS
-        // A constructor is a special method for creating objects of the same type, and it assigns the key/value pairs.
-       // Parameters are passed into constructor rather than the class declaration
-
-//The COMMAND OBJECT will always include a commandType property
-//Some commandTypes will be couple with a vaule property, nut not all
-//Every COMMAND object is a single instruction to be delivered to the rover
-
-
-
 const Command = require("./command");
 
 class Message {
 
   constructor(name, commands) {
-  //   commands = [
-  //     new Command('MOVE'),
-  //     new Command('STATUS_CHECK'),
-  //     new Command('MODE_CHANGE', 'LOW_POWER'),
-  //  ];
     this.name = name;
     if (!name) {
       throw Error("Message name required.");
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,35 +8,33 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-// TEST 7-PASSED-----2/25 2:09PM
-// “constructor sets position and default values for mode and generatorWatts”.
-//  Refer to the Rover Class description above for these default values.
+// TEST 7
 
 it('constructor sets position and default values for mode and generatorWatts', function () {
-    let testRover = new Rover(98382);
-    expect(testRover.position).toEqual(98382);
+    let testRover = new Rover(4321);
+
+    expect(testRover.position).toEqual(4321);
     expect(testRover.mode).toEqual('NORMAL');
     expect(testRover.generatorWatts).toEqual(110);
 })
 
-// TEST 8-PASSED-----2/25 2:34PM, UPDATED 2/26 9:51AM-----Restructured the return to use Message in function
+// TEST 8
 
 it('response returned by receiveMessage contains the name of the message', function () {
     let commandsTest = new Command('STATUS_CHECK');
     let messageTest = new Message('test for status check', commandsTest);
-    let testRoverName = new Rover().receiveMessage(messageTest);
-    let testRoverNameReturn = testRoverName.message;
-    // console.log(testRoverName.message);
+    let testRover = new Rover().receiveMessage(messageTest);
+    let testRoverNameReturn = testRover.message;
+
     expect(testRoverNameReturn).toBe('test for status check');
 })
 
-// TEST 9-PASSED-----2/25 2:56PM, UPDATED 2/26 10:08AM-----Restructured return of test 9, two empty objects. Both 8 and 9 are passing together.
+// TEST 9
 
     it ('response returned by receiveMessage includes two results if two commands are sent in the message', function () {
         let commandsTest = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
         let messageTest = new Message('Test message with two commands', commandsTest);
-        let testRover = new Rover().receiveMessage(messageTest);
-        // console.log(testRover.results);
+        let testRover = new Rover(4321).receiveMessage(messageTest);
         let testRoverReturn = testRover.results;
         let testRoverResults = [
             {
@@ -44,33 +42,26 @@ it('response returned by receiveMessage contains the name of the message', funct
             },
             {
                completed: true, 
-               roverStatus: { mode: 'LOW_POWER', generatorWatts: 110, position: this.position }
+               roverStatus: { mode: 'LOW_POWER', generatorWatts: 110, position: 4321 }
             }
          ] 
-        //   console.log(testRover.results);
+    
         expect(testRoverReturn).toEqual(testRoverResults)
  
     })
 
-// TEST 10-passed...need other tests to confirm. Completed: true? How?
-// 1. For the STATUS_CHECK command, receiveMessage(message).results includes a roverStatus object
-//    describing the current state of the rover object — mode, generatorWatts, and position. 
-//    The test should check each of these for accuracy.
-// 2. See the Rover Command Types table for more details.
+// TEST 10
 
 it('responds correctly to the status check command', function () {
     let commandsTest = [new Command('STATUS_CHECK')];
     let messageTest = new Message('Status check test', commandsTest);
-    let testRover = new Rover().receiveMessage(messageTest).results;
-    let testRoverResults = [{completed: true, roverStatus: {mode: 'NORMAL', generatorWatts: 110, position: this.position}}]
-    // console.log(testRover);
+    let testRover = new Rover(4321).receiveMessage(messageTest).results;
+    let testRoverResults = [{completed: true, roverStatus: {mode: 'NORMAL', generatorWatts: 110, position: 4321}}]
 
     expect(testRover).toEqual(testRoverResults)
 }); 
 
-// TEST 11-PASSED-----2/26 6:11PM
-// 1. The test should check the completed: property and rover mode for accuracy.
-// 2. The rover has two modes that can be passed as values to a mode change command: ‘LOW_POWER’ and ‘NORMAL’.
+// TEST 11
 
 it('responds correctly to the mode change command', function() {
 
@@ -79,15 +70,12 @@ it('responds correctly to the mode change command', function() {
     let messageTest = new Message('Mode change test', commandsTest);
     let testRoverMessage = testRover.receiveMessage(messageTest);
     let testRoverResults = testRoverMessage.results
-    // console.log(testRover);
+    
     expect(testRoverResults).toEqual([{completed: true}]);
     expect(testRover.mode).toEqual('LOW_POWER');
 });
 
 // TEST 12
-// “responds with a false completed value when attempting to move in LOW_POWER mode”
-// 1. The test should check the completed property for accuracy and confirm that the rover’s position did not change.
-// 2. Use the Rover Modes table for guidance on how to handle move commands in different modes.
 
 it ('responds with a false completed value when attempting to move in LOW_POWER mode', function() {
     
@@ -96,21 +84,16 @@ it ('responds with a false completed value when attempting to move in LOW_POWER 
     let commandsTest = [new Command('MOVE', 6000)];
     let messageTest = new Message('Low power false test', commandsTest);
     let testRoverMessage = testRover.receiveMessage(messageTest).results;
-    // console.log(testRover);
+    
     expect(testRoverMessage).toEqual([{completed: false}])
 })
 
 
 
 //TEST 13
-//“responds with the position for the move command”
-//1. A MOVE command will update the rover’s position with the position value in the command.
 
 it ('responds with the position for the move command', function () {
-    // let commandsTest = [new Command('MOVE', 4321)];
-    // let messageTest = new Message('Move command test', commandsTest);
-    // let testRover = new Rover(1111).receiveMessage(messageTest);
-    // expect(testRover.postion).toEqual(4321);
+    
     let testRover = new Rover(4321);
     testRover.mode = 'NORMAL'
     let commandsTest = [new Command('MOVE', 6000)];
